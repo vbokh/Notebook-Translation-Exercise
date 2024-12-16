1ed2251cd9c018da979c34893e54dc0b92ec8f55
vbokh <vdb30@georgetown.edu>
2024-12-16 01:27:27 -0500

    Removing duplicates

--- app/Stock_Report.py
+++ app/Stock_Report.py
@@ -7,6 +7,7 @@ from plotly.express import line
 
 
 from app.alpha_service import API_KEY
+
 def fetch_stocks_csv(symbol):
     request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&outputsize=full&datatype=csv"
     df = read_csv(request_url)
