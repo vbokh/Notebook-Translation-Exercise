3a269c92cec82339e82a7a6714802143cb295adc
vbokh <vdb30@georgetown.edu>
2024-12-16 01:33:33 -0500

    update on duplication

--- app/Stock_Report.py
+++ app/Stock_Report.py
@@ -5,8 +5,7 @@
 from pandas import read_csv
 from plotly.express import line
 
-
-from app.alpha_service import API_KEY
+from alpha_service import API_KEY
 
 def fetch_stocks_csv(symbol):
     request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&outputsize=full&datatype=csv"
