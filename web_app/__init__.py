c7d56cd01dacce6d77b011993c146de7c86e26bb
vbokh <vdb30@georgetown.edu>
2024-12-17 06:00:25 -0500

    Flash Messaging

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,12 +1,14 @@
 # this is the "web_app/__init__.py" file...
-
+import os 
 from flask import Flask
 
 from web_app.routes.home_routes import home_routes
 from web_app.routes.stocks_routes import stocks_routes
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret")
 
 def create_app():
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = SECRET_KEY
     app.register_blueprint(home_routes)
     app.register_blueprint(stocks_routes)
     return app
@@ -14,3 +16,4 @@ def create_app():
 if __name__ == "__main__":
     my_app = create_app()
     my_app.run(debug=True)
+
