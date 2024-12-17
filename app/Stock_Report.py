fdbc44b53cb374b14e319ae72982964a3f091f90
vbokh <vdb30@georgetown.edu>
2024-12-17 06:09:41 -0500

    Web (#6)
    * setup flask app with basic routing
    
    * render html templates
    
    * pass vars from route to page
    
    * shared layout
    
    * implemented bootstrap styles
    
    * Stocks dashboard with form
    
    * Flash Messaging

--- app/Stock_Report.py
+++ app/Stock_Report.py
@@ -1,56 +1,82 @@
+
+# this is the "app/stocks.py" file...
+
+# IMPORTS
+
 from pandas import read_csv
 from plotly.express import line
-from alpha_service import API_KEY  # Importing API_KEY, dotenv setup handled there
 
-def fetch_stocks_csv(symbol):
+from app.alpha_service import API_KEY
+
+# FUNCTIONS
+
+def format_usd(my_price):
+    return f"${float(my_price):,.2f}"
+
+
+def fetch_stocks_csv(symbol="MSFT"):
     request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&outputsize=full&datatype=csv"
-    df = read_csv(request_url)
-    return df
+    return read_csv(request_url)
+
+
+# the weird main conditional says
+# only run the indented code if you are running this file
+# from the command line
+# otherwise if importing from this file, ignore the stuff below
 
 if __name__ == "__main__":
-    # SELECT A SYMBOL
-    symbol = input("Please input a symbol (e.g. 'NFLX'): ") or "NFLX"
+
+    # only if running from command line will this get reached
+
+    # STOCK SELECTION
+
+    symbol = input("Please input a symbol (e.g. 'NFLX'): ")
     print("SYMBOL:", symbol)
 
-    # FETCH THE DATA
+    # REPORT
+
     df = fetch_stocks_csv(symbol)
 
     print(df.columns)
     print(len(df))
-    print(df.head())
+    df.head()
 
     # Challenge A
     #
     # What is the most recent adjusted closing price? And the corresponding date?
+    # Display the price formatted as USD, with dollar sign and two decimal places.
+
     print("-------------------------")
     print("LATEST CLOSING PRICE:")
     first_row = df.iloc[0]
-    print(f"${first_row['adjusted_close']} as of {first_row['timestamp']}")
+    #print(first_row)
+    print(f"${first_row['adjusted_close']}", "as of", first_row["timestamp"])
+
 
     # Challenge B
     #
     # What is the average, median, min, and max adjusted closing price
     # (over the latest 100 available days only)?
-    recent_df = df.iloc[0:100]  # Use slicing or df.head(100)
+
+    recent_df = df.iloc[0:100] # use slicing or df.head(100)
     print(len(recent_df))
 
     print("-------------------------")
     print("RECENT STATS...")
-    print(f"MEAN PRICE: ${recent_df['adjusted_close'].mean():.2f}")
-    print(f"MEDIAN PRICE: ${recent_df['adjusted_close'].median():.2f}")
-    print(f"MIN PRICE: ${recent_df['adjusted_close'].min():.2f}")
-    print(f"MAX PRICE: ${recent_df['adjusted_close'].max():.2f}")
-    # Quantiles for additional insight
-    print(f"75TH PERCENTILE: ${recent_df['adjusted_close'].quantile(.75):.2f}")
-    print(f"25TH PERCENTILE: ${recent_df['adjusted_close'].quantile(.25):.2f}")
+    print(f"MEAN PRICE: ${recent_df['adjusted_close'].mean()}")
+    print(f"MEDIAN PRICE: ${recent_df['adjusted_close'].median()}")
+    print(f"MIN PRICE: ${recent_df['adjusted_close'].min()}")
+    print(f"MAX PRICE: ${recent_df['adjusted_close'].max()}")
+    # quantiles, for fun :-)
+    print(f"75TH PERCENTILE: ${recent_df['adjusted_close'].quantile(.75).round(2)}")
+    print(f"25TH PERCENTILE: ${recent_df['adjusted_close'].quantile(.25).round(2)}")
 
     # Challenge C
     #
     # Plot a line chart of adjusted closing prices over time (all time).
-    fig = line(
-        x=df["timestamp"],
-        y=df["adjusted_close"],
-        title=f"Stock Prices ({symbol})",
-        labels={"x": "Date", "y": "Stock Price ($)"},
-    )
+
+
+    fig = line(x=df["timestamp"], y=df["adjusted_close"],
+                title=f"Stock Prices ({symbol})",
+            labels= {"x": "Date", "y": "Stock Price ($)"})
     fig.show()
